6b8e1586f5e0fc0bfd637ff034cd415714448b88
Nil Meruliya <100191375+NilMeruliya@users.noreply.github.com>
2023-10-11 07:50:30 -0400

    set up tailwind css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { ChatIcon } from './svg'
 
 const App = () => {
   return (
-    <div>
-      hello
-      <ChatIcon />
-    </div>
+    <>
+    <div className='dark'></div>
+    </>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js, jsx, ts, tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        dark_bg1: "#111B21",
+        dark_bg2: "#202C33",
+        dark_bg3: "#182229",
+        dark_bg4: "#222E35",
+        dark_bg5: "#233138",
+        dark_bg6: "#101A20",
+        dark_text1: "#E9EDEF",
+        dark_text2: "#8696A0",
+        dark_text3: "#8696a0",
+        dark_text4: "#D1D6D8",
+        dark_text5: "#99BEB7",
+        dark_border1: "#222D34",
+        dark_border2: "#313D45",
+        dark_svg1: "#AEBAC1",
+        dark_svg2: "#8696A0",
+        dark_hover1: "#2A3942",
+        blue1: "#53BDEB",
+        blue2: "#3E7B96",
+        green1: "#00A884",
+        green2: "#008069",
+        green3: "#005C4B",
+        green4: "#025144",
+        dark_scrollbar: "#374045",
+      },
+    },
+  },
+  plugins: [],
+}
+
